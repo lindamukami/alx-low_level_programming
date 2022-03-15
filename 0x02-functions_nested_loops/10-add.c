19be7806b50ade5628b8f8cd1113f2941c60d426
lindamukami <lindamukami32@gmail.com>
2022-03-15 19:11:10 +0000

    10-add task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.c"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	
+	return (c);
+}
