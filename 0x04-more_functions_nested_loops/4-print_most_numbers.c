203ef2df327d6972d5d6e10f71d173533fd07d49
lindamukami <lindamukami32@gmail.com>
2022-03-17 16:05:55 +0000

    added task 4 to 10

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,15 +7,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	i = 0;
-
-	while (i <= 9)
+	for (i = 0; i < 10; i++)
 	{
-		if (i == 2 && i == 4)
+		if (i != 2 && i != 4)
 		{
 			_putchar(i + '0');
 		}
-		i++;
 	}
 
 	_putchar('\n');
