c4c9dc185e7fe4e30d6ff8af343603961ffd79ba
lindamukami <lindamukami32@gmail.com>
2022-03-28 20:48:48 +0000

    added task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: c or NULL
+ */
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
+}
