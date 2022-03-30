53116f4af958fd6ed28016847ffe2834af78fe2f
lindamukami <lindamukami32@gmail.com>
2022-03-30 19:47:22 +0000

    added task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,6 +2,7 @@
 /**
  * _strlen_recursion - function that returns the length of a string
  * @s: string to return
+ * Return: length of a string
  */
 int _strlen_recursion(char *s)
 {
@@ -9,5 +10,5 @@ int _strlen_recursion(char *s)
 	{
 		return (0);
 	}
-	return (1 + _strlen_recursion(s+1));
+	return (1 + _strlen_recursion(s + 1));
 }
