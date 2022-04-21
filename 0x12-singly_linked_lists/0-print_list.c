c32c65d37cd5399cc7c616ae41765d38c88e4ec7
lindamukami <lindamukami32@gmail.com>
2022-04-21 21:16:04 +0000

    added task 0 to 4

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *print_list - prints all elements of a list_t list
+ *@h:list to print
+ *Return:no. of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
