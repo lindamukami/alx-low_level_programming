3793c966fb1cd4621add3ad979bbeb65ec1fee96
lindamukami <lindamukami32@gmail.com>
2022-04-24 07:39:17 +0000

    added comments

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,12 +14,18 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
+	{
 		return (NULL);
+	}
+	/* for loop to get length of str*/
 	for (len = 0; str[len]; len++)
-		;
-	new_node->len = len;
-	new_node->str = strdup(str);
-	new_node->next = (*head);
-	(*head) = new_node;
+	{
+		new_node->len = len;
+		new_node->str = strdup(str);
+		/* point next of new_node to head*/
+		new_node->next = (*head);
+		/* make head point to new_node*/
+		(*head) = new_node;
+	}
 	return (new_node);
 }
