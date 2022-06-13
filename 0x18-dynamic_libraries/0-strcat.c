ef9a672232a9ae218c189b0141ebec4aae146485
lindamukami <lindamukami32@gmail.com>
2022-06-13 19:24:55 +0000

    dynamic files

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_strcat - concatenates two strings
+ *
+ *@dest:destination
+ *@src:source
+ *
+ *Return:char
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
